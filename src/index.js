a4457fed3a257c7e23277c531843b35e45c919dc
max5961 <mmorrissey5961@gmail.com>
2023-06-24 01:27:51 -0400

    perm link

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './style.css';
-// const image = document.createElement('img');
-// image.src = '../dist/docs/assets/images/image-test.jpg';
-// document.querySelector('#content').appendChild(image);
+const image = document.createElement('img');
+image.src = 'https://github.com/max5961/webpack-images-test/blob/7228a1e77d8320d141d99db6b50a36755c2b2439/dist/docs/assets/images/image-test.jpg';
+document.querySelector('#content').appendChild(image);
